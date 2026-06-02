b86e4fdf1b005e1dc3d497ea3e75cbaef20e7efb
Carlsan <carlsankim.dev@gmail.com>
2026-06-02 15:05:32 +0800

    [3.19] Final changes on custom main activity layout

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -4,40 +4,44 @@ import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.aboutme.databinding.ActivityCustomMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCustomMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_custom_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_custom_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.custom_main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        findViewById<Button>(R.id.done_btn).setOnClickListener {
+
+        binding.doneBtn.setOnClickListener {
             addNickname(it)
         }
-
 
     }
 
     fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        nickNameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nickNameTextView.visibility = View.VISIBLE
+        binding.apply {
+            val editText = binding.nicknameEdit
+            val nickNameTextView = binding.nicknameText
+            invalidateAll()
+            nickNameTextView.text = editText.text
+            editText.visibility = View.GONE
+            view.visibility = View.GONE
+            nickNameTextView.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
